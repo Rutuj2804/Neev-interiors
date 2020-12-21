771c336e71fc8e5b299bfae24f5fe17c142a6afd
Rutuj2804 <bokaderutuj36@gmail.com>
2020-12-21 17:07:24 +0530

    updated for hosting

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('single/', views.single, name='single'),
     path('contact/', views.contact, name='contact'),
     path('blog/', views.blogs, name='blogs'),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
